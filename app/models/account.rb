744e8446ecf57d7a5ab9c39e45de9797e30ba117
Nicholas Schneider <nicholasschneider@Nicholass-MacBook-Pro.local>
2017-10-01 21:31:43 -0500

    Added checks so blank user names and passwords cannot be added. Also linked the mailer to the new account creation. It does not work yet to my knowledge but it is a work in progress

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,9 @@
 class Account < ApplicationRecord
+
+    validates_presence_of :name
+    validates_presence_of :password
+
+    def create
+        NewAccountMailer.submitted('nick').deliver_later
+    end
 end
