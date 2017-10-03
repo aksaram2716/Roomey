62f5908f70d80ca37e48ba73c6ce20f8573b589a
Nicholas Schneider <nicholasschneider@Nicholass-MacBook-Pro.local>
2017-10-03 17:50:51 -0500

    Created a signup page

--- db/migrate/20171002014608_create_accounts.rb
+++ db/migrate/20171002014608_create_accounts.rb
@@ -13,6 +13,7 @@ class CreateAccounts < ActiveRecord::Migration[5.1]
     create_table :users do |t|
         t.string :name
         t.string :email
+        t.string :phone
 
         t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171003191441) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
