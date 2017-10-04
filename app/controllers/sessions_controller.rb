bfb475ca52fed66b3ec9d31b7a6e6abdcc3062d9
Nicholas Schneider <nicholasschneider@pal-nat186-5-24.itap.purdue.edu>
2017-10-03 20:54:59 -0500

    Login page worksgit add -A Tests are not fully passing yet though

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    account = Account.find_by(email: params[:session][:email].downcase)
-    flash.now[:notice] = account
-    if account && account.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      log_in accounts
-      redirect_to account
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
-      # Create an error message.
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
