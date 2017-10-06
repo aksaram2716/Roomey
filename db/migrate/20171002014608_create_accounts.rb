607624363db8555cda26c551218ca961d0d00d52
Nicholas Schneider <nicholasschneider@pal-nat186-7-172.itap.purdue.edu>
2017-10-06 13:50:42 -0500

    preparing wyatt's for the merge with the new mater

--- db/migrate/20171002014608_create_accounts.rb
+++ db/migrate/20171002014608_create_accounts.rb
@@ -1,15 +1,5 @@
 class CreateAccounts < ActiveRecord::Migration[5.1]
   def change
-    create_table :accounts do |t|
-      t.string :name
-      t.string :password
-      t.string :email
-      t.string :phone
-      t.string :address
-
-      t.timestamps
-    end
-
     create_table :users do |t|
         t.string :name
         t.string :email
--- db/migrate/20171006180605_create_homes.rb
+++ db/migrate/20171006180605_create_homes.rb
@@ -3,7 +3,7 @@ class CreateHomes < ActiveRecord::Migration[5.1]
     create_table :homes do |t|
       t.string :name
       t.string :address
-      t.references :Account, foreign_key: true
+      t.references :User, foreign_key: true
 
       t.timestamps
     end
