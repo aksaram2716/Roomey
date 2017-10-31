2e7b21b59471ce39af29f48e90a1f1e5d6cf9df0
aksaram2716 <agudoor@purdue.edu>
2017-10-31 14:05:30 -0700

    Homes Commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -64,12 +64,12 @@ class HomesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_home
-      @home = Home.find(params[:uid])
+      @home = Home.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def home_params
-      params.require(:home).permit(:name, :address, :user_uid)
+      params.require(:home).permit(:name, :address, :user_id)
 
     end
 end
