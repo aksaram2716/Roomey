f7f403e2ca81d379074334643115df9683a6e410
AtbTheGreat <annatiffanybenjamin@gmail.com>
2017-11-03 02:02:32 -0400

    Created the ruby files for conducting payments

---
+++ app/controllers/payment_controller.rb
@@ -0,0 +1,20 @@
+class PaymentController < ApplicationController
+
+
+  def pay:
+  end
+
+
+  def refund:
+  end
+
+  def cancel_transaction:
+  end
+
+  def change_vendor:
+  end
+
+  def change_currency:
+  end
+
+end
